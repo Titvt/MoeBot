9835d0caddb74eb0cf0b717e1ac21e513d6a3fe3
Titvt <tqydyqt@126.com>
2024-09-14 16:29:57 +0800

    add unknown tags notification

--- src/plugins/v2ex/__init__.py
+++ src/plugins/v2ex/__init__.py
@@ -6,11 +6,11 @@ from nonebot.rule import is_type
 from requests import get
 
 
-def parse_element(element: HtmlElement) -> str:
+def parse_element(element: HtmlElement, unknown_tags: set) -> str:
     content = (element.text or "").strip()
 
     for i in element:
-        content += f"{parse_element(i)}{(i.tail or '').strip()}"
+        content += f"{parse_element(i, unknown_tags)}{(i.tail or '').strip()}"
 
     match element.tag:
         case "a" | "div" | "h2" | "h3" | "h4" | "h5" | "h6" | "ol" | "pre" | "ul":
@@ -22,16 +22,18 @@ def parse_element(element: HtmlElement) -> str:
         case "code" | "p":
             return f"{content}\n"
         case _:
+            unknown_tags.add(element.tag)
             return ""
 
 
-def parse_v2ex(url: str) -> tuple[str, str]:
+def parse_v2ex(url: str) -> tuple[str, str, set[str]]:
     response = get(url)
     tree = html.fromstring(response.text)
     title = tree.xpath("//h1/text()")[0]
     body = tree.xpath("//div[@class='markdown_body']")[0]
-    content = parse_element(body)
-    return title, content
+    unknown_tags = set()
+    content = parse_element(body, unknown_tags)
+    return title, content, unknown_tags
 
 
 cmd_msg = on_message(is_type(GroupMessageEvent))
@@ -44,9 +46,14 @@ async def fn_msg(event: GroupMessageEvent):
     if not message.startswith("https://www.v2ex.com/t/"):
         return
 
-    title, content = parse_v2ex(message)
+    title, content, unknown_tags = parse_v2ex(message)
 
     if len(content) > 1024:
         content = f"{content[:1024]}\n\n..."
 
-    await cmd_msg.send(f"标题：\n{title}\n\n内容：\n{content}")
+    message = f"标题：\n{title}\n\n内容：\n{content}"
+
+    if unknown_tags:
+        message += f"\n\n发现未知标签，请联系管理员：\n{unknown_tags}"
+
+    await cmd_msg.send(message)
