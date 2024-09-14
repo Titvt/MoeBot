32468130c54205bbb7b6b8f094592ce06b846311
Titvt <tqydyqt@126.com>
2024-09-14 17:04:31 +0800

    add legal check

--- src/plugins/v2ex/__init__.py
+++ src/plugins/v2ex/__init__.py
@@ -1,9 +1,16 @@
 from lxml import html
 from lxml.html import HtmlElement
-from nonebot import on_message
+from nonebot import get_driver, on_message
 from nonebot.adapters.onebot.v11 import GroupMessageEvent
 from nonebot.rule import is_type
+from openai import OpenAI
 from requests import get
+
+config = get_driver().config
+chat_client = OpenAI(
+    api_key=config.api_key,
+    base_url=config.base_url,
+)
 
 
 def parse_element(element: HtmlElement, unknown_tags: set) -> str:
@@ -30,10 +37,32 @@ def parse_v2ex(url: str) -> tuple[str, str, set[str]]:
     response = get(url)
     tree = html.fromstring(response.text)
     title = tree.xpath("//h1/text()")[0]
-    body = tree.xpath("//div[@class='markdown_body']")[0]
+    body = tree.xpath("//div[@class='topic_content']")[0]
     unknown_tags = set()
     content = parse_element(body, unknown_tags)
     return title, content, unknown_tags
+
+
+def legal_check(content: str) -> bool:
+    return (
+        chat_client.chat.completions.create(
+            model="gpt-4o",
+            messages=[
+                {
+                    "role": "system",
+                    "content": "你是一个负责检查文章是否合法合规的程序，你会将用户发送的文本当作程序的输入，当文章的标题和内容不包含任何违法违规内容时输出<true>，否则输出<false>，'<'和'>'也是输出内容的一部分，除了上述的两种输出，不能出现任何其他的输出）",
+                },
+                {
+                    "role": "user",
+                    "content": content,
+                },
+            ],
+            temperature=0,
+        )
+        .choices[0]
+        .message.content
+        == "<true>"
+    )
 
 
 cmd_msg = on_message(is_type(GroupMessageEvent))
@@ -52,6 +81,10 @@ async def fn_msg(event: GroupMessageEvent):
         content = f"{content[:1024]}\n\n..."
 
     message = f"标题：\n{title}\n\n内容：\n{content}"
+
+    if not legal_check(message):
+        await cmd_msg.send("该链接内容疑似违法违规，请勿点击！")
+        return
 
     if unknown_tags:
         message += f"\n\n发现未知标签，请联系管理员：\n{unknown_tags}"
